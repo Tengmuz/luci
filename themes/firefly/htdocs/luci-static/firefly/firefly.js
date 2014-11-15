4ab4c95707ac1d8a145529894e0d7665522b131a
wengbj <wbj@t-chip.com.cn>
2014-11-15 14:15:57 +0800

    Theme:修复Firefly主题布局bug

--- themes/firefly/htdocs/luci-static/firefly/firefly.js
+++ themes/firefly/htdocs/luci-static/firefly/firefly.js
@@ -1,5 +1,15 @@
 
 $(document).ready(function(){
+    $("input[type=file]").wrap('<span class="input_file_context  cbi-button"></span>');
+    $("input[type=file]").wrap('<span class="input_file_real"></span>');
+    $(".input_file_context").append('<span class="input_file_fake cbi-button cbi-input-file">Selected ...</span>');
     
+    $("input[type=file]").change(function(){
+        $(this).parent().parent().children(".input_file_fake").html($(this).val())
+    })
+ 
+    //some button define with:100px ， after we changed the theme , the text cannot display fully
+    //let the text display full
+    $(".cbi-button").attr("style","");
 });
 
